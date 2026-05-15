944f0bf721c8cfa6754c40c3794fbebda557cf80
urshofer <u.hofer@tr51.org>
2026-05-15 15:54:27 +0200

    First Commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(448, 256);
+  fullscreen(true);
+}
+
+function draw() {
+  background(220);
+  line(0, 0, width, height);
+  line(0, height, width, 0);
+}
+
+function keyPressed() {
+  if (key === 'f' || key === 'F') {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
